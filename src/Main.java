92c8233e889cd0d5446e5a23dfe39caae7367b72
Mikel Hinojosa <boras728@hotmail.com>
2024-04-09 10:59:31 -0600

    agrgar archivoEjemplo

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       String chichenitza="chichenitza";
+        System.out.println("chichenitza =" + chichenitza.replace("i","0"));
+        System.out.println("chichenitza =" + chichenitza);
+        System.out.println("chichenitza.indexOf('t') =" + chichenitza.indexOf('t'));
+        System.out.println("chichenitza.lastIndexOf('i') =" + chichenitza.lastIndexOf('i'));
+        System.out.println("chichenitza.indexOf('h') =" + chichenitza.indexOf('h'));
+        System.out.println("chichenitza.contains('itza') =" + chichenitza.contains("itza"));
+        System.out.println("chichenitza.startsWith(\"cho\") =" + chichenitza.startsWith("cho"));
+        System.out.println("chichenitza.startsWith(\"zaza\") =" + chichenitza.endsWith("zaza"));
+        System.out.println("          chichenitza              ");
+        System.out.println(" chichenitza ".trim());
+
+
+    }
+}
