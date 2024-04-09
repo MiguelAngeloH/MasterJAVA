d21b8156d649c0fde73a538b228c96584e90f9d5
Mikel Hinojosa <boras728@hotmail.com>
2024-04-09 12:01:48 -0600

    cmd prueba02

--- src/EjemploStringExtensionArchivo.java
+++ src/EjemploStringExtensionArchivo.java
@@ -1,2 +1,9 @@
 public class EjemploStringExtensionArchivo {
+    public static void main(String[] args) {
+        String archivo = "alguna_imagen.com";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("archivo.substring(archivo.length()-4)= " + archivo.substring(i+1));
+
+    }
 }
