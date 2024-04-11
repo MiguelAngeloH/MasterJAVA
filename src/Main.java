56d0a4022b5c004fb9ea25eaaa350aa9ae037d98
Mikel Hinojosa <boras728@hotmail.com>
2024-04-11 09:17:10 -0600

    add Methods 11-abril ok

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       String chichenitza="chichenitza";
-        System.out.println("chichenitza =" + chichenitza.replace("i","0"));
-        System.out.println("chichenitza =" + chichenitza);
-        System.out.println("chichenitza.indexOf('t') =" + chichenitza.indexOf('t'));
-        System.out.println("chichenitza.lastIndexOf('i') =" + chichenitza.lastIndexOf('i'));
-        System.out.println("chichenitza.indexOf('h') =" + chichenitza.indexOf('h'));
-        System.out.println("chichenitza.contains('itza') =" + chichenitza.contains("itza"));
-        System.out.println("chichenitza.startsWith(\"cho\") =" + chichenitza.startsWith("cho"));
-        System.out.println("chichenitza.startsWith(\"zaza\") =" + chichenitza.endsWith("zaza"));
-        System.out.println("          chichenitza              ");
-        System.out.println(" chichenitza ".trim());
-
+      String trenmaya = "trenmaya";
+        System.out.println("trenmaya.length = "+trenmaya.length());
+        System.out.println("trenmaya.toChartArray()="+trenmaya.toCharArray());
+char [] array = trenmaya.toCharArray();
+int largo = array.length;
+        System.out.println("Largo = " +largo);
+        for (int i = 0; i < largo; i++) {
+            System.out.println("arreglo ="+ array[i]);
+        }
 
     }
 }
