9389831d0a102f2957e6f29c2d80fcae50df3e7d
Mikel Hinojosa <boras728@hotmail.com>
2024-04-16 08:50:40 -0600

    add 16 abril martes

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,25 @@ char [] array = trenmaya.toCharArray();
 int largo = array.length;
         System.out.println("Largo = " +largo);
         for (int i = 0; i < largo; i++) {
-            System.out.println("arreglo ="+ array[i]);
+            System.out.println("arreglo = "+ array[i]);
+        }
+        System.out.println("trenmaya = "+ trenmaya.split("a"));
+        System.out.println("\n");
+
+        String[] array02 = trenmaya.split("a");
+        int largo02 = array02.length;
+        for (int j = 0; j <largo02 ; j++) {
+            System.out.println(array02[j]);
         }
 
+        String archivo = "alguna.imagen.pdf";
+        String[] archivoArr = archivo.split("\\.");
+        //String[] archivoArr = archivo.split("[a]");
+        largo02 = archivoArr.length;
+        System.out.println("largo02"+largo02);
+        for (int j = 0; j <largo02 ; j++) {
+            System.out.println(archivoArr[j]);
+        }
+        System.out.println("extension=" +archivoArr[largo02-1] );
     }
 }
